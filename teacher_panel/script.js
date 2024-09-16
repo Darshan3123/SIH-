42c46103ef6ed7f80d171df556408f15459e80da
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-09-16 12:22:49 +0530

    fully responsive admin/teacher panel

--- teacher_panel/script.js
+++ teacher_panel/script.js
@@ -3,3 +3,7 @@ const toggleSidebar = document.getElementById('toggleSidebar');
 toggleSidebar.addEventListener('click', () => {
   sidebar.classList.toggle('-translate-x-full');
 });
+
+document.getElementById('profileDropdown').addEventListener('click', function () {
+  document.getElementById('dropdownMenu').classList.toggle('hidden');
+});
